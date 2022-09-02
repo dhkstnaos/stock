9e169dc13170c3430272b1b7eca868e297e9381b
dhkstnaos <dhkstnaos@gmail.com>
2022-09-02 15:29:38 +0900

    feat: 비관적락

--- src/main/java/com/example/stock/service/PessimisticRockStockService.java
+++ src/main/java/com/example/stock/service/PessimisticRockStockService.java
@@ -1,16 +1,24 @@
 package com.example.stock.service;
 
+import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class PessimisticRockService {
+public class PessimisticRockStockService {
 
   private final StockRepository stockRepository;
 
-  public PessimisticRockService(StockRepository stockRepository) {
+  public PessimisticRockStockService(StockRepository stockRepository) {
     this.stockRepository = stockRepository;
   }
 
-  public void PessimisticRockStockService
+  @Transactional
+  public void decrease(Long id, Long quantity) {
+    Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+    stock.decrease(quantity);
+
+    stockRepository.saveAndFlush(stock);
+  }
 }
